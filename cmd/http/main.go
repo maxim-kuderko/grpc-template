2a90b2dfe35967face6fa712d10f3a257dc271aa
Max Kuderko <kuderko@gmail.com>
2021-02-25 14:58:38 +0200

    + go mod

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -15,10 +15,6 @@ import (
 
 func main() {
 	app := fx.New(
-		// Provide all the constructors we need, which teaches Fx how we'd like to
-		// construct the *log.Logger, http.Handler, and *http.ServeMux types.
-		// Remember that constructors are called lazily, so this block doesn't do
-		// much on its own.
 		fx.Provide(
 			initializers.NewConfig,
 			initializers.NewMetricsAndTracer,
@@ -28,12 +24,6 @@ func main() {
 			newHandler,
 			router,
 		),
-		// Since constructors are called lazily, we need some invocations to
-		// kick-start our application. In this case, we'll use Register. Since it
-		// depends on an http.Handler and *http.ServeMux, calling it requires Fx
-		// to build those types using the constructors above. Since we call
-		// NewMux, we also register Lifecycle hooks to start and stop an HTTP
-		// server.
 		fx.Invoke(webserver),
 	)
 
